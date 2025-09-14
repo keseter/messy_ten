bc62cba90f2835771a0f883ef3df0df576b75c2f
keseter <worang19@gmail.com>
2025-09-14 15:30:13 +0700

    Attemp 3 fixing

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from main.views import show_main, create_product, show_product, show_xml, show_json, show_xml_by_id, show_json_by_id
-import uuid
 
+print("testig")
 app_name = 'main'
  # Urls and Views
  # When a user clicks a link such as a href = ... ex in the tempalte html
@@ -10,7 +10,7 @@ app_name = 'main'
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product/', create_product, name='create_product'),
-    path('product/<uuid:id>/', show_product, name='show_product'),
+    path('product/<str:id>/', show_product, name='show_product'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<str:product_id>/', show_xml_by_id, name='show_xml_by_id'),
