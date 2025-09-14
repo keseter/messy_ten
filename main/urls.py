7c334a1c4f3f551056e9358d7fb1eae68e0d0a55
keseter <worang19@gmail.com>
2025-09-14 15:28:44 +0700

    Attempt 2 fixing id dan uuid

--- main/urls.py
+++ main/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('product/<uuid:id>/', show_product, name='show_product'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
-    path('xml/<uuid:product_id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<uuid:product_id>/', show_json_by_id, name='show_json_by_id'),
+    path('xml/<str:product_id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:product_id>/', show_json_by_id, name='show_json_by_id'),
 ]
